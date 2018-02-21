3a11f6c7972c62e6be28f5432cd6cfd0d7a9ba12
André Duarte <up201405639@fe.up.pt>
2018-02-21 10:55:38 +0000

    fixed push/pop sequence

--- CGRA_1/tp1/MyTable.js
+++ CGRA_1/tp1/MyTable.js
@@ -17,27 +17,27 @@ class MyTable extends CGFobject
 
   display() {
     this.scene.pushMatrix();
-    this.scene.pushMatrix();
-    this.scene.pushMatrix();
-    this.scene.pushMatrix();
 
     this.scene.translate(2.5, 3.5, 1.5);
     this.scene.scale(5, 0.3, 3);
     this.cover.display();
 
     this.scene.popMatrix();
+    this.scene.pushMatrix();
 
     this.scene.translate(0.5, 1.75, 0.5);
     this.scene.scale(0.3, 3.5, 0.3);
     this.leg1.display();
 
     this.scene.popMatrix();
+    this.scene.pushMatrix();
 
     this.scene.translate(0.5, 1.75, 3-0.5);
     this.scene.scale(0.3, 3.5, 0.3);
     this.leg2.display();
 
     this.scene.popMatrix();
+    this.scene.pushMatrix();
 
     this.scene.translate(5-0.5, 1.75, 0.5);
     this.scene.scale(0.3, 3.5, 0.3);
