8532cc5ef1bb11408ef04b7b433d3e0e1aa49006
André Duarte <up201405639@fe.up.pt>
2018-03-07 11:06:16 +0000

    added poop colour

--- CGRA_2/tp2/MyQuad.js
+++ CGRA_2/tp2/MyQuad.js
@@ -4,25 +4,32 @@
  */
 class MyQuad extends CGFobject
 {
-	constructor(scene) 
+	constructor(scene)
 	{
 		super(scene);
 
 		this.initBuffers();
 	};
 
-	initBuffers() 
+	initBuffers()
 	{
 		this.vertices = [
-		-0.5, -0.5, 0,
-		0.5, -0.5, 0,
-		-0.5, 0.5, 0,
-		0.5, 0.5, 0
+			-0.5, -0.5, 0,
+			0.5, -0.5, 0,
+			-0.5, 0.5, 0,
+			0.5, 0.5, 0
 		];
 
 		this.indices = [
-		0, 1, 2, 
-		3, 2, 1
+			0, 1, 2,
+			3, 2, 1
+		];
+
+		this.normals = [
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, 1,
 		];
 
 		this.primitiveType = this.scene.gl.TRIANGLES;
--- CGRA_2/tp2/MyTable.js
+++ CGRA_2/tp2/MyTable.js
@@ -4,14 +4,24 @@
  */
  class MyTable extends CGFobject
  {
-	constructor(scene) 
+	constructor(scene)
 	{
 		super(scene);
 
 		this.myUnitCubeQuad = new MyUnitCubeQuad(this.scene);
+
+    this.init();
 	};
 
-	display() 
+  init() {
+		this.scene.materialWood = new CGFappearance(this.scene);
+		this.scene.materialWood.setSpecular(0.1,0.1,0.1,1);
+		//this.scene.materialWood.loadTexture("http://192.168.56.1:8080/CGRA_2/textures/wood.jpg"); //requires activation of textures and vertice mapping
+		this.scene.materialWood.setAmbient(0.509, 0.321, 0.003,1);
+		this.scene.materialWood.setDiffuse(0.509, 0.321, 0.003,1);
+  }
+
+	display()
 	{
 		// legs
 		this.scene.pushMatrix();
@@ -38,12 +48,14 @@
 		this.myUnitCubeQuad.display();
 		this.scene.popMatrix();
 
+    //this.materialDefault.apply();
+
 		// table top
 		this.scene.pushMatrix();
 		this.scene.translate(0, 3.5, 0);
 		this.scene.scale(5, 0.3, 3);
+    this.scene.materialWood.apply();
 		this.myUnitCubeQuad.display();
 		this.scene.popMatrix();
 	};
  };
-
